010235d9398feb8e4880f509d42933011ac91360
RootX <ebafrost@gmail.com>
2026-03-15 06:55:58 +0300

    Delete src directory

--- src/components/ui/GlassCard.jsx
+++
@@ -1,7 +0,0 @@
-export function GlassCard({ children, className = '', as: Tag = 'article', ...props }) {
-  return (
-    <Tag className={`glass-card ${className}`.trim()} {...props}>
-      {children}
-    </Tag>
-  );
-}
--- src/constants/externalLinks.js
+++
@@ -1,5 +0,0 @@
-export const EXTERNAL_LINKS = {
-  mbtiTest: 'https://www.16personalities.com/free-personality-test',
-  riasecTest: 'https://www.mynextmove.org/explore/ip',
-  contactEmail: 'mailto:hello@careercompass.app'
-};
